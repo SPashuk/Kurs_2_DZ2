d5ed3ed9de1f8a9a1e2d41b83cea2574f9f4b232
Сергей Пашук <133160380+SPashuk@users.noreply.github.com>
2023-08-15 22:02:23 +0500

    Fixed 1

--- Script-2.sql
+++ Script-2.sql
@@ -7,18 +7,36 @@ FROM song
 WHERE track_time = (SELECT MAX(track_time) FROM song);
 
 -- 2. Название треков, продолжительность которых не менее 3,5 минут.
-SELECT track_name, track_time FROM song
-WHERE track_time > '3:30:00'
+SELECT track_name, track_time 
+FROM song
+WHERE track_time >= '00:03:30'
 ORDER BY track_time DESC;
 
 -- 3. Названия сборников, вышедших в период с 2018 по 2020 год включительно.
-select collection_name from collections
+select collection_name 
+from collections
 where collection_year between 2018 and 2020;
 
 -- 4. Исполнители, чьё имя состоит из одного слова.
-select first_name, last_name from musician
+select first_name--, last_name, nick_name 
+from musician
 where nick_name not like '% %';
 
--- 5. Название треков, которые содержат слово «мой» или «my».
+-- 5. Название треков, которые содержат слово «мой» или «my». вариант 1+++
 select track_name from song
-where track_name like '%мой%' OR track_name like '%my%';
+where track_name ilike 'мой'
+OR track_name ilike 'мой %'
+OR track_name ilike '% мой %'
+OR track_name ilike '% мой'
+OR track_name ilike 'my'
+OR track_name ilike 'my %'
+OR track_name ilike '% my %'
+OR track_name ilike '% my';
+
+-- 5. Название треков, которые содержат слово «мой» или «my». вариант 2
+select track_name from song/* Запрашиваем имена треков из таблицы треков */
+where string_to_array(lower(track_name), ' ')&&array['my', 'мой'];
+
+-- 5. Название треков, которые содержат слово «мой» или «my» - вариант 3.
+select track_name from song/* Запрашиваем имена треков из таблицы треков */
+where track_name ~* '\mmy\M' or track_name ~* '\mмой\M';
--- Script-3.sql
+++ Script-3.sql
@@ -6,8 +6,16 @@ select genre_id, (
 	where id=genre_id) as g1,
 		COUNT(mus_gen_id) 
 as musician_id 
-from musician_genre 
+from musician_genre
 group by genre_id order by genre_id;--desc;
+
+--1. Количество исполнителей в каждом жанре. вариант 2++
+select genre_name, count(musician_id) 
+from genre g 
+left join musician_genre mg on g.id = mg.genre_id 
+group by genre_name/* Группируем по айди жанров - не прокатило, только по имени */
+order by genre_name;/* Сортируем по имени */
+
 --2. Количество треков, вошедших в альбомы 2019–2020 годов.++
 select count(track_name) from albums a
 join song s on a.id = s.albums_id
@@ -23,7 +31,18 @@ order by mean;
 select nick_name, album_year from musician m 
 join musician_album ma on m.id = ma.albums_id
 join albums a on ma.albums_id = a.id
-where album_year <> 2020;
+where album_year  != 2020;--<> 2020;/*не равен 2020*/
+
+--4. Все исполнители, которые не выпустили альбомы в 2020 году. вариант2
+SELECT nick_name /* Получаем имена исполнителей */
+from musician  /* Из таблицы исполнителей */
+where nick_name NOT IN ( /* Где имя исполнителя не входит в вложенную выборку */
+    select nick_name /* Получаем имена исполнителей */
+    from musician m /* Из таблицы исполнителей */
+    join musician_album ma on m.id = ma.albums_id /* Объединяем с промежуточной таблицей */
+    join albums a on ma.albums_id = a.id /* Объединяем с таблицей альбомов */
+    where album_year = 2020 /* Где год альбома равен 2020 */
+);
 
 --5. Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).++
 select distinct collection_name from collections c
@@ -33,6 +52,7 @@ join musician_album ma on s.albums_id = ma.albums_id
 join musician m on ma.musician_id = m.id
 where nick_name = 'А. Розенбаум'
 order by collection_name;-- desc; -- 'А. Розенбаум';
+
 --5. Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).+
 select collection_name from collections c
 where c.id in (select collect_id from collection_track ct 
